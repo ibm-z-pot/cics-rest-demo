a758cb830d7cad1df328343171cf94af7c2d7b9c
ibm-z-pot <steve_foley@uk.ibm.com>
2024-02-19 11:06:41 +0000

    Initial Commit

---
+++ cics-rest-demo/src/main/java/com/ibmzpot/cics/rest/demo/SystemApplication.java
@@ -0,0 +1,8 @@
+package com.ibmzpot.cics.rest.demo;
+
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+
+@ApplicationPath("system")
+public class SystemApplication extends Application {
+}
